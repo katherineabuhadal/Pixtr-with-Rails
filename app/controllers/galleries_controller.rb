eb80896fa56cba7baac0ea20f7f6bf5eaa4b3807
katherineabuhadal <katherine8@verizon.net>
2014-06-16 14:56:00 -0400

    add gallery

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,6 +1,20 @@
 class GalleriesController < ApplicationController
+	
 	def index
 		@galleries = Gallery.all
+	end
+
+	def show
+		@gallery = Gallery.find(params[:id])
+	end
+
+	def new
+		@gallery = Gallery.new
+	end
+
+	def create
+		gallery = Gallery.create(params[:gallery])
+		redirect_to "/galleries/#{gallery.id}"
 	end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/" => "galleries#index"
+  get "/galleries/new" => "galleries#new"
+  get "/galleries/:id" => "galleries#show"
+  post "/galleries" => "galleries#create"
+
 end
