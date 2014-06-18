8ef3895b6cd893fde792a27c056de21aae0334f8
katherineabuhadal <katherine8@verizon.net>
2014-06-18 13:39:34 -0400

    image validation

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,30 +4,38 @@ class ImagesController < ApplicationController
 		@image = Image.new
 	end
 
+	def create
+		@image = Image.new(image_params)
+		@gallery = Gallery.find(params[:gallery_id])
+
+		if @image.save
+			redirect_to @gallery
+		else
+			render :new
+		end
+	end
+
 	def edit
 		@gallery = Gallery.find(params[:gallery_id])
 		@image = Image.find(params[:id])
 		
 	end
 
-	def create
-		image = Image.create(image_params)
-		gallery = Gallery.find(params[:gallery_id])
-		redirect_to "/galleries/#{gallery.id}"
-	end
-
 	def update
-		gallery = Gallery.find(params[:gallery_id])
-		image = Image.find(params[:id])
-		image.update(image_params)
-		redirect_to "/galleries/#{gallery.id}"
+		@gallery = Gallery.find(params[:gallery_id])
+		@image = Image.find(params[:id])
+		if @image.update(image_params)
+			redirect_to @gallery
+		else
+			render :edit
+		end
 	end
 
 
 	def destroy
 		gallery = Gallery.find(params[:gallery_id])
 		image = Image.find(params[:id])
-		gallery.image.destroy
+		image.destroy
 		redirect_to "/galleries/#{gallery.id}"
 	end
 
