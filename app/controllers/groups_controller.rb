987963fb8f04756a9cf10a2822279d7dd1c06eee
katherineabuhadal <katherine8@verizon.net>
2014-06-24 15:17:06 -0400

    Added list of my groups and auto enroll in group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params) 
+
     if @group.save
+      GroupMembership.create(
+        user_id: current_user.id,
+        group_id: @group.id
+      )
       redirect_to :groups
     else
       render :new
@@ -14,13 +19,14 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.all
+    @my_groups = current_user.groups
     @group_membership = GroupMembership.new
   end
 
   private
 
-    def group_params
-      params.require(:group).permit(:name, :description)
-    end
-
+  def group_params
+    params.require(:group).permit(:name, :description)
   end
+
+end
